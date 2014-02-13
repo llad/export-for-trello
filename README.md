988c327f6af920d0aff9042b321b3a7ca917e766
Trapias <trapias@gmail.com>
2014-02-13 13:26:02 +0100

    TrelloExport fork 1.8.8
    1. export Trello Plus Spent and Estimate values, extracting values from
    card titles in format (S/E)
    2. export comments (with a default limit of 100, see commentLimit in
    script)
    3. export checklists and checklists' items
    4. export list of attachments
    5. export a link to each card
    6. export votes
    7. use updated version of xlsx.js, modified by me (escapeXML)
    8. use updated version of jquery, 2.1.0
    9. use usernames instead of initials for members

--- README.md
+++ README.md
@@ -1,38 +1,80 @@
 TrelloExport
 ============
 
-Chrome extension to export data from Trello.
+Chrome extension to export data from Trello to Excel.
 
-Install Published Version
--------------------------
-Grab it from the [Chrome Web Store](https://chrome.google.com/webstore/detail/trelloexport/nhdelomnagopgaealggpgojkhcafhnin?hl=en).
+##Fork
+This is a fork of the original TrelloExport extension, available at [https://github.com/llad/export-for-trello](https://github.com/llad/export-for-trello).
 
 
-Install Development Version
----------------------------
+###Fork modifications
+1. export Trello Plus Spent and Estimate values, extracting values from card titles in format (S/E)
+2. export comments (with a default limit of 100, see commentLimit in script)
+3. export checklists and checklists' items
+4. export list of attachments
+5. export a link to each card
+6. export votes
+7. use updated version of xlsx.js, modified by me (escapeXML)
+8. use updated version of jquery, 2.1.0
+9. use usernames instead of initials for members
+
+---
+
+####Notes
+I modified the **escapeXML** function in **xlsx.js** to avoid errors with XML characters when loading the spreadsheet in Excel. I tested exporting quite big boards like Trello Development or Trello Resources and no more have issues with invalid characters.
+I put a couple of sample export files in the xlsx subfolder.
+
+**Columns**: the list of columns exported is now:
+
+	columnHeadings = ['List', 'Title', 'Link', 'Description', 'Checklists', 'Comments', 'Attachments', 'Votes', 'Spent', 'Estimate', 'Due', 'Members', 'Labels']
+
+---
+
+####Formatting
+I tried formatting data in a readable format, suggest changes if you don't like how it is now.
+
+**Comments** are formatted with [date - username] comment, e.g.:
+
+	[2014-01-31 18:38:31 - kathyschultz1] the add-on for exporting to Excel is a good start, but I'm w/all who dream of a report that includes checklists and comments. Thanks Trello warriors!
+
+I added **commentLimit** to limit the number of comments to extract: play with the value (default 100) as per your needs.
+There are currently no limits in the number of checklists, checklist items or attachments.
+
+**Attachments** are listed in a similar way, with [filename] (bytes) url, e.g.:
+
+	[chrome.jpg] (62806) https://trello-attachments.s3.amazonaws.com/4d5ea62fd76aa1136000000c/520a29971618ecef3c002181/dc1d95c904a04a6a986b775e55f58bd9/chrome.jpg
+
+**Excel formatting**: after opening the excel you will have to adjust columns widths and formatting. I normally align cells on top and wrap text to have a readable format - see the samples in xlsx.
+
+---
+
+###How to install
+This fork is not on the Chrome Web Store, but you can manually install it by following these steps:
 1. Download the repository as a zip file
 2. Extract zip
 3. Go to Chrome Exensions: [chrome://chrome/extensions/](chrome://chrome/extensions/)
 4. Click on Developer Mode checkbox
 5. Click on Load unpacked extension...
 6. Select the folder containing the source files
+7. Reload Trello
 
-Usage
------
+---
+
+###How to use
 1. From a board, click on the board title
 2. Click on Share, print, and export...
 3. Click on Export Excel
 
-Credit
-------
-Inspired by some non-functioning code I found hiding in [TrelloScrum](https://github.com/Q42/TrelloScrum/).
+---
+
+###What's next?
+I wish to add a configuration dialog to set some options before exporting, e.g. to choose which lists to export, to set the limit onn the number of comments to extract etc.
+Suggestions are welcome!
 
 
-Notes
------
-Watch out for that xlsx.js license from Microsoft.
+---
 
-Fork Whatsnew
------
-1. export cpoints to a new column
-
+##Original TrelloExport
+###Install Published Version
+Grab the original extension from the [Chrome Web Store](https://chrome.google.com/webstore/detail/trelloexport/nhdelomnagopgaealggpgojkhcafhnin?hl=en).
+See the original Guthub repository at [https://github.com/llad/export-for-trello](https://github.com/llad/export-for-trello).
--- trelloexport.js
+++ trelloexport.js
@@ -5,33 +5,52 @@
  * Credit:
  * Started from: https://github.com/Q42/TrelloScrum
  * 
- * Modified by A.Velo
+ * Forked by @trapias (Alberto Velo)
  * https://github.com/trapias/trelloExport
  * Whatsnew:
-	- export cpoints to a new column
-	
+	- export Trello Plus Spent and Estimate data
+	- export checklists
+	- export comments (see commentLimit, default 100)
+	- export attachments
+	- export votes
+	- use updated version of xlsx.js, modified by me (escapeXML)
+	- use updated version of jquery, 2.1.0
  */
 
 // Variables
 var $excel_btn,
-    columnHeadings = ['List', 'Title', 'Description', 'Points', 'CPoints', 'Due', 'Members', 'Labels'];
+    columnHeadings = ['List', 'Title', 'Link', 'Description', 'Checklists', 'Comments', 'Attachments', 'Votes', 'Spent', 'Estimate', 'Due', 'Members', 'Labels'],
+	commentLimit = 100; // limit the number of comments to put in the spreadsheet
 
 window.URL = window.webkitURL || window.URL;
 
 // on DOM load
 $(function () {
-    
+ //  $(window).load(function () { 
+   
+   //todo: fix loading when side menu is collapsed
+   
+   setTimeout(function(){
     // Look for clicks on the .js-share class, which is
     // the "Share, Print, Export..." link on the board header option list
-    $('.js-share').live('mouseup', function () {
+    $('.js-share').on('mouseup', function () {
         setTimeout(addExportLink);
     });
+   }, 1000);
+   
+   //bind when opening side menu
+   $('a.sidebar-show-btn').on('click', function(){
+   //console.log('clicked sidebar');
+	$('.js-share').on('mouseup', function () {
+        setTimeout(addExportLink);
+    });
+   });
+   
 });
 
 
 // Add a Export Excel button to the DOM and trigger export if clicked
 function addExportLink() { 
-   
     var $js_btn = $('a.js-export-json'); // Export JSON link
     
     // See if our Export Excel is already there
@@ -53,19 +72,16 @@ function addExportLink() {
 
 function createExcelExport() {
 
-    // RegEx to find the points for users of TrelloScrum
-    var pointReg = /[\(](\x3f|\d*\.?\d+)([\)])\s?/m;
-	var cpointReg = /[\[](\x3f|\d*\.?\d+)([\]])\s?/m;
+    // RegEx to find the Trello Plus Spent and Estimate (spent/estimate) in card titles
+    var SpentEstRegex = /\(([0-9]+(\.[0-9]+)?)\/?([0-9]+(\.[0-9]+)?)\)?/;
 	
-	/*
-		TrelloScrum
-		reg = /[\(](\x3f|\d*\.?\d+)([\)])\s?/m, //parse regexp- accepts digits, decimals and '?', surrounded by ()
-		regC = /[\[](\x3f|\d*\.?\d+)([\]])\s?/m, //parse regexp- accepts digits, decimals and '?', surrounded by []
-	*/
+	console.log('Start export...');
 	
+	//var dlg = "<div id=\"dialog\" title=\"Basic dialog\"><p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p></div>";
+	//$(dlg).dialog();
 	
     $.getJSON($('a.js-export-json').attr('href'), function (data) {
-            
+	
         var file = {
             worksheets: [[],[]], // worksheets has one empty worksheet (array)
             creator: 'TrelloExport',
@@ -77,7 +93,10 @@ function createExcelExport() {
             
             // Setup the active list and cart worksheet
             w = file.worksheets[0]; 
-            w.name = data.name;
+			if(data.name.length>30)
+				w.name = data.name.substr(0,30);
+			else
+				w.name = data.name;
             w.data = [];
             w.data.push([]);
             w.data[0] = columnHeadings;
@@ -85,7 +104,12 @@ function createExcelExport() {
             
             // Setup the archive list and cart worksheet            
             wArchived = file.worksheets[1]; 
-            wArchived.name = 'Archived ' + data.name;
+			wArchived.name = 'Archived cards';
+			/* if(data.name.length>11)
+				wArchived.name = 'Archived ' + data.name.substr(0,11);
+			else
+				wArchived.name = 'Archived ' + data.name; */
+			
             wArchived.data = [];
             wArchived.data.push([]);
             wArchived.data[0] = columnHeadings;
@@ -105,28 +129,45 @@ function createExcelExport() {
                 if (card.idList == list_id) {
                     var title = card.name;
 					
-					//points for estimated hours
-                    var parsed = title.match(pointReg);
-                    var points = parsed ? parsed[1] : '0';
-                    title = title.replace(pointReg, '');
-                    
-					if(points=='?') points='0';
+				console.log('TITLE ' + title);
+				var spent=0; var estimate=0;
+				var checkListsText='';
+				var commentsText=''; var commentCounter=0;
+				var attachmentsText='';
+				
+				//Trello Plus Spent/Estimate
+				//TODO: fix!!!
+				var spentData = title.match(SpentEstRegex);
+				if(spentData!=null)
+				{
+					/* console.log('spentData=' + spentData);
+					console.log('spentData[1]=' + spentData[1]);
+					console.log('spentData[2]=' + spentData[2]);
+					console.log('spentData[3]=' + spentData[3]);
+					console.log('spentData[4]=' + spentData[3]); */
+					spent = spentData[1];
+					estimate = spentData[3];
 					
-					//cpoints for worked hours
-					var cparsed = title.match(cpointReg);
-                    var cpoints = cparsed ? cparsed[1] : '0';
-                    title = title.replace(cpointReg, '');
+					if(spent==undefined) spent=0;
+					if(estimate==undefined) estimate=0;
 					
-					/* console.log('title=' + title);
-					console.log('points=' + points);
-					console.log('cpoints=' + cpoints); */
-					
+					//console.log('SPENT ' + spent + ' / estimate ' + estimate);
+				}
+				else
+				{
+					//no spent info found, do nothing
+					//console.log('UNSPENT ' + spent + ' / estimate ' + estimate);
+					spent=0;
+					estimate=0;
+				}
+				
+				title = title.replace(SpentEstRegex, '');
+				title = title.replace('() ', '');
 					
                     // tag archived cards
                     if (card.closed) {
                         title = '[archived] ' + title;
                     }
-                    
                     var due = card.due || '';
                     
                     //Get all the Member IDs
@@ -135,7 +176,8 @@ function createExcelExport() {
                     $.each(memberIDs, function (i, memberID){
                         $.each(data.members, function (key, member) {
                             if (member.id == memberID) {
-                                memberInitials.push(member.initials);
+                                //memberInitials.push(member.initials);
+								memberInitials.push(member.username);
                             }
                         });
                     });
@@ -151,20 +193,100 @@ function createExcelExport() {
                         }
 
                     });
-                    
+					
+					//all checklists
+					var checklists = [];
+					$.each(card.idChecklists, function (i, idchecklist){
+					    if (idchecklist) {
+                            checklists.push(idchecklist);
+                        }
+                    });
+                    				
+					//parse checklists
+					console.log('parse ' + checklists.length + ' checklists for this card');
+					//var checkListsText='';
+					$.each(checklists, function(i, checklistid){
+					//console.log('PARSE ' + checklistid);
+						 $.each(data.checklists, function (key, list) {
+							var list_id = list.id;
+							//console.log('found ' + list_id);
+							if(list_id == checklistid)
+							{
+								//console.log('CHECKLIST ' + list.name);
+								checkListsText += list.name + ':\n';
+								//checkitems
+								$.each(list.checkItems, function (i, item){
+									if (item) {
+										//console.log('item ' + item.name + ' in state ' + item.state);
+										checkListsText += ' - ' + item.name + ' (' + item.state + ')\n';
+									}
+								});
+								
+								//checkListsText += '\n';
+							}
+						
+						});
+						
+					});
+
+					//comments
+					if(data.actions)
+					{
+						//Date.js parsing, in progress
+						//var shortDate = Date.CultureInfo.formatPatterns.shortDate;
+
+						console.log('parse ' + data.actions.length + ' comments for this card');
+						$.each(data.actions, function(j, action){  
+						
+							 if(action.type == "commentCard" && commentCounter <= commentLimit){
+								if(card.id == action.data.card.id){
+								commentCounter ++;
+								//2013-08-08T06:57:18 
+								//var sActionDate = action.date.substr(0,19); //.toString( shortDate );
+								var sActionDate = action.date.substr(0,10) + ' ' + action.date.substr(11,8);
+								
+								//console.log('sActionDate ' + sActionDate);
+								//var sdate = Date.parse(sActionDate);
+								
+								if(action.memberCreator)
+								{
+									commentsText += '[' + sActionDate + ' - ' + action.memberCreator.username + '] ' + action.data.text + "\n";
+								}
+								else{
+									commentsText += '[' + sActionDate + '] ' + action.data.text + "\n";
+								}
+								
+									
+								}
+							} 
+						});
+					}
+					
+					if(card.attachments)
+					{
+						console.log('parse ' + card.attachments.length + ' attachments for this card');
+						$.each(card.attachments, function(j, attach){  
+							 attachmentsText += '[' + attach.name + '] (' + attach.bytes + ') ' + attach.url + '\n';
+						});
+					}
+					 
                     // Need to set dates to the Date type so xlsx.js sets the right datatype
                     if (due !== '' ){
                         var d = new Date(due);
                         due = d;
                     }
                     
-					
                     var rowData = [
                             listName,
                             title,
+							'https://trello.com/c/' + card.id,
                             card.desc,
-                            points,
-							cpoints,
+							checkListsText,
+							commentsText,
+							attachmentsText,
+							card.idMembersVoted.length,
+                            spent,
+							estimate,
                             due,
                             memberInitials.toString(),
                             labels.toString()
@@ -185,6 +307,8 @@ function createExcelExport() {
             });
         });
         
+		console.log('Prepare xlsx...');
+		
         // We want just the base64 part of the output of xlsx.js
         // since we are not leveraging they standard transfer process.
         byteString = window.atob(xlsx(file).base64);
@@ -201,9 +325,11 @@ function createExcelExport() {
             type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         });
         var board_title = data.name;
+		
         saveAs(blob, board_title + '.xlsx');
         $("a.close-btn")[0].click();
 
+		console.log('Done exporting ' + board_title + '.xlsx');
 
     });
 
